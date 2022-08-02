8a57055aa2f021a7c5af372247437adf9ad1559d
w <pcranaway@tuta.io>
2022-08-03 00:06:59 +0300

    make "reactive" rendering better

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,6 +1,10 @@
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread::sleep,
+    time::Duration,
 };
 
 use self::ui::UI;
@@ -10,14 +14,24 @@ pub mod ui;
 pub fn init_renderer() -> Arc<AtomicBool> {
     let ui = UI::new();
 
-    let should_render: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let should_render: Arc<AtomicBool> = Arc::new(AtomicBool::new(true));
     let should_render_clone = should_render.clone();
 
     tokio::spawn(async move {
         loop {
-            if true == should_render_clone.load(Ordering::Relaxed) {
+            let result = should_render_clone.compare_exchange(
+                true,
+                false,
+                Ordering::Relaxed,
+                Ordering::Relaxed,
+            );
+
+            if result.is_ok() {
                 ui.render();
             }
+
+            // TODO: Find a better way to reactively update
+            sleep(Duration::from_millis(50));
         }
     });
 
